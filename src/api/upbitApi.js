1880874b8d2f419cb43890f3d4fe63b74ec531a6
KimTyun <106860407+KimTyun@users.noreply.github.com>
2025-09-11 14:49:02 +0900

    [refector] api 수정

--- src/api/upbitApi.js
+++ src/api/upbitApi.js
@@ -11,23 +11,34 @@ const upbitApi = axios.create({
 
 // 코인 리스트 가져오기, 호가 상위 n개를 가져온다. 기본 15개
 export const getTickerAll = async (n = 15) => {
-   const response = await upbitApi.get('ticker/all', {
-      params: {
-         quote_currencies: 'KRW',
-      },
-   })
+   try {
+      const response = await upbitApi.get('ticker/all', {
+         params: {
+            quote_currencies: 'KRW',
+         },
+      })
 
-   return response.data.sort((a, b) => Number(b.trade_price) - Number(a.trade_price)).slice(0, n)
+      return response.data.sort((a, b) => Number(b.trade_price) - Number(a.trade_price)).slice(0, n)
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
 }
 
 // 코인 이름 가져오기(코인 한글 이름을 가져오는 용도)
 export const getMarketAll = async () => {
-   const response = await upbitApi.get('market/all', {
-      params: {
-         is_details: false,
-      },
-   })
-   return response.data
+   try {
+      const response = await upbitApi.get('market/all', {
+         params: {
+            is_details: false,
+         },
+      })
+
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
 }
 
 // 캔들차트 데이터 가져오기
@@ -39,6 +50,7 @@ export const getCandles = async (time = 'days', params) => {
             converting_price_unit: 'KRW',
          },
       })
+
       return response.data
    } catch (error) {
       if (env === 'development') console.error(error)
