2882710db5b0c53c690e8b67fbe86ecc0ce0c7dd
KimTyun <106860407+KimTyun@users.noreply.github.com>
2025-09-16 18:05:40 +0900

    [feat] userAPI 추가
    todo: 백엔드 userAPI 완성, userpage에 데이터 바인딩

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import newsSlice from '../features/newsSlice.js'
 import boardSlice from '../features/boardSlice.js'
 import coinSlice from '../features/coinSlice.js'
-
+import userSlice from '../features/userSlice.js'
 const store = configureStore({
    reducer: {
       news: newsSlice,
       coin: coinSlice,
       board: boardSlice,
+      user: userSlice,
    },
 })
 
