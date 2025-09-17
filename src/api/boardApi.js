f3e0ae8eee1343a10ccf0488776f284aac742dc9
taemin2336 <165637767+taemin2336@users.noreply.github.com>
2025-09-17 18:24:35 +0900

    [feat]: 게시글좋아요구현, 댓글 좋아요90% 구현

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -1,71 +1,84 @@
-import axiosApi from ".";
-const env = import.meta.env.VITE_ENV;
+import axiosApi from '.'
+const env = import.meta.env.VITE_ENV
 
 // 게시글 리스트 가져오기
 export const getBoard = async (category) => {
-  try {
-    const response = await axiosApi.get("/board", {
-      params: {
-        category: category,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.get('/board', {
+         params: {
+            category: category,
+         },
+      })
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 게시글 등록하기
 export const writeBoard = async (boardData) => {
-  try {
-    const response = await axiosApi.post("/board/write", boardData, {
-      // headers로 감싸서 폼데이터 형식으로 보냄
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.post('/board/write', boardData, {
+         // headers로 감싸서 폼데이터 형식으로 보냄
+         headers: {
+            'Content-Type': 'multipart/form-data',
+         },
+      })
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 특정 게시글 가져오기
 export const getBoardById = async (id) => {
-  try {
-    const response = await axiosApi.get(`/board/${id}`);
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.get(`/board/${id}`)
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 게시글 삭제
 export const deleteBoard = async (id) => {
-  try {
-    const response = await axiosApi.delete(`/board/${id}`);
-    return response;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.delete(`/board/${id}`)
+      return response
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 게시글 수정
 export const updateBoard = async (id, boardData) => {
-  try {
-    const response = await axiosApi.put(`/board/${id}`, boardData, {
-      // headers로 감싸서 폼데이터 형식으로 보냄
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.put(`/board/${id}`, boardData, {
+         // headers로 감싸서 폼데이터 형식으로 보냄
+         headers: {
+            'Content-Type': 'multipart/form-data',
+         },
+      })
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
+
+// 게시글 추천
+export const likeBoard = async (id, userId = null) => {
+   try {
+      const response = await axiosApi.post(`/board/${id}/like`, {
+         user_id: userId,
+      })
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
--- src/api/commentApi.js
+++ src/api/commentApi.js
@@ -1,40 +1,48 @@
-import axiosApi from ".";
-const env = import.meta.env.VITE_ENV;
+import axiosApi from '.'
+const env = import.meta.env.VITE_ENV
 
 // 댓글 등록
 export const createComment = async (commentData) => {
-  try {
-    const response = await axiosApi.post(
-      `/board/${commentData.board_id}/comment`,
-      commentData
-    );
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.post(`/board/${commentData.board_id}/comment`, commentData)
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 댓글 가져오기
 export const listComment = async (boardId) => {
-  try {
-    const response = await axiosApi.get(`/board/${boardId}/comments`);
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.get(`/board/${boardId}/comments`)
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
 
 // 댓글 삭제
 export const deleteComment = async ({ boardId, commentId }) => {
-  try {
-    const response = await axiosApi.delete(
-      `/board/${boardId}/comment/${commentId}`
-    );
-    return response.data;
-  } catch (error) {
-    if (env === "development") console.error(error);
-    throw error;
-  }
-};
+   try {
+      const response = await axiosApi.delete(`/board/${boardId}/comment/${commentId}`)
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
+
+// 댓글 추천
+export const likeComment = async (id, userId = null) => {
+   try {
+      const response = await axiosApi.post(`/board/${id}/comment/like`, {
+         user_id: userId,
+      })
+      return response.data
+   } catch (error) {
+      if (env === 'development') console.error(error)
+      throw error
+   }
+}
