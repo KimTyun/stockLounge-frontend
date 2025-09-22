3523087f927ad2b3e0f270b4a2f608ee6350ea70
silvergraylap <tlgjadyd12@gmail.com>
2025-09-22 10:48:41 +0900

    [fix]:하드코딩된 URL 수정
    [fix]:하드코딩된 URL 수정

--- src/components/auth/SocialLogin/SocialLogin.jsx
+++ src/components/auth/SocialLogin/SocialLogin.jsx
@@ -5,7 +5,7 @@ import { FaGoogle, FaComment } from 'react-icons/fa'
 const SocialLogin = ({ provider }) => {
    useEffect(() => {
       const handleMessage = (event) => {
-         if (event.origin !== 'http://localhost:8000' && event.origin !== 'http://localhost:5173') return
+         if (event.origin !== 'APP_API_URL' && event.origin !== 'https://stocklounge.store/api') return
          if (event.data?.success) {
             if (event.data.redirectUrl) {
                window.location.href = event.data.redirectUrl
